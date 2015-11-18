237d70365fac90a8f850f8a4aded5be5c1bd1cea
GlukKazan <glukkazan@gmail.com>
2015-11-18 18:21:30 +0300

    [Axiom] Puluc (utils)

--- utils/perl/puluc.pl
+++ utils/perl/puluc.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 
 my %nodes;
+my %names;
 my %values;
 my %links;
 
@@ -26,10 +27,11 @@ for (my $i = 0; $i < 0b11110000; $i++) {
      if ($n <= 4 && $k <= 4 && $s) {
          $nodes{$r}  = $s;
          $values{$r} = $i;
+         $names{$i}  = $r;
      }
 }
 
-foreach $x (keys %nodes) {
+foreach my $x (keys %nodes) {
      my $y = $x;
      $y =~ tr/WB/BW/;
      if ($nodes{'W' . $y}) {
@@ -46,12 +48,98 @@ foreach $x (keys %nodes) {
      }
 }
 
+printf "DEFER\tmove-piece\n";
+printf "DEFER\tclear-dice\n";
+printf "DEFER\tdrop-dice\n\n";
+
+printf "{move-priorities\n";
+printf "\t{move-priority} normal-type\n";
+printf "\t{move-priority} pass-type\n";
+printf "move-priorities}\n\n";
+
 printf "{moves move-pieces\n";
-printf 		"\t{move} move-piece {move-type} normal\n";
+printf "\t{move} move-piece\t{move-type} normal-type\n";
+printf "\t{move} Pass\t\t{move-type} pass-type\n";
+printf "moves}\n\n";
+
+printf "{moves clear-dices\n";
+printf "\t{move} clear-dice\t{move-type} clear-type\n";
+printf "moves}\n\n";
+
+printf "{moves drop-dices\n";
+printf "\t{move} drop-dice\t{move-type} dice-type\n";
 printf "moves}\n\n";
 
 printf "{pieces\n";
-foreach $x (sort keys %nodes) {
+printf "\t{piece} mark\t{moves} clear-dices\n";
+printf "\t{piece} zero\t{moves} drop-dices\t{value} 0\n";
+printf "\t{piece} one\t{moves} drop-dices\t{value} 1\n";
+foreach my $x (sort keys %nodes) {
 	printf "\t{piece} $nodes{$x}\t{moves} move-pieces\t{value} $values{$x}\n";
 }
-printf "pieces}\n";
+printf "pieces}\n\n";
+
+sub lpad {
+	my ($str, $len, $chr) = @_;
+	$chr = " " unless (defined($chr));
+	return substr(($chr x $len) . $str, -1 * $len, $len);
+}
+
+printf ": puluc-head ( -- piece-type )\n";
+printf "\tpiece piece-value mark\n";
+foreach my $value (sort keys %names) {
+	my $name = $names{$value};
+	my $node = $nodes{$name};
+	my $res  = substr($node, 0, 1);
+	$value   = lpad($value, 4);
+	printf "\tOVER $value = IF DROP $res ENDIF\n";
+}
+printf "\tSWAP DROP\n";
+printf "\tDUP mark > verify\n";
+printf ";\n\n";
+
+printf ": puluc-tail ( -- piece-type )\n";
+printf "\tpiece piece-value mark\n";
+foreach my $value (sort keys %names) {
+	my $name = $names{$value};
+	my $node = $nodes{$name};
+	my $res  = substr($node, 1);
+	if ($res) {
+		$res   = lpad($res, 5);
+		$value = lpad($value, 4);
+		printf "\tOVER $value = IF DROP $res ENDIF\n";
+	}
+}
+printf "\tSWAP DROP\n";
+printf ";\n\n";
+
+sub puluc_cons {
+	my ($piece) = @_;
+	printf ": $piece-cons ( -- piece-type )\n";
+	printf "\tpiece piece-value mark\n";
+	foreach my $value (sort keys %names) {
+		my $name = $names{$value};
+		my $node = $nodes{$name};
+		if ($links{$name}->{$piece}) {
+			$res   = lpad($links{$name}->{$piece}, 5);
+			$value = lpad($value, 4);
+			printf "\tOVER $value = IF DROP $res ENDIF\n";
+		}
+	}
+	printf "\tSWAP DROP\n";
+	printf ";\n\n";
+}
+
+puluc_cons('a');
+puluc_cons('b');
+puluc_cons('c');
+puluc_cons('d');
+
+printf ": puluc-cons ( piece-type -- piece-type )\n";
+printf "\tDUP a = IF a-cons ENDIF\n";
+printf "\tDUP b = IF b-cons ENDIF\n";
+printf "\tDUP c = IF c-cons ENDIF\n";
+printf "\tDUP d = IF d-cons ENDIF\n";
+printf "\tSWAP DROP\n";
+printf "\tDUP mark > verify\n";
+printf ";\n";
